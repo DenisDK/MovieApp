4e8eeb12c8378446c66df96564ae59d8b329bea3
Kushniruk Denis <denys.kushniruk@nure.ua>
2024-04-22 21:05:00 +0300

    Fectch data from tmdb API in the home page

---
+++ src/components/Results.jsx
@@ -0,0 +1,12 @@
+
+export const Results = ({ results }) => {
+   return (
+      <div>
+         {results.map((result) => (
+            <div key={result.id}>
+               <h2>{result.original_title}</h2>
+            </div>
+         ))}
+      </div>
+   )
+}
